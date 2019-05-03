712d11a65540ea6ff0789f7595a616c98936bd3b
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-03 18:10:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function nameToAttributes(arr) {
 
 function attributesToPhrase(drivers) {
   return drivers.map(function (driver) {
-    return `$(driver.name) is from $(driver.hometown)`
+    return `${driver.name} is from ${driver.hometown}`
   });
 }
