7722271f0f23d36e59bdbbfcf0650da3568ba99c
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-03 18:06:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nameToAttributes(arr) {
     const driverFirst = driver.split(" ")[0];
     const driverLast = driver.split(" ")[1];
     
-    return { firstName: driverFirst, lastNamer: driverLast };
+    return { firstName: driverFirst, lastName: driverLast };
   });
 }
